d012526063d40078c7a489196fa78dafa6581d24
cfzngcfz <cfzngcfz@163.com>
2024-03-23 20:40:58 +0800

    20240323

--- py/9.7.seq2seq.py
+++ py/9.7.seq2seq.py
@@ -5,7 +5,7 @@ from torch import nn
 from torch.utils import data
 
 # 1. 读取数据集 & 构建数据迭代器和词表
-# 9.5.1  下载和预处理数据集
+# source: 9.5.1  下载和预处理数据集
 DATA_HUB = dict()
 DATA_URL = 'http://d2l-data.s3-accelerate.amazonaws.com/'
 DATA_HUB['fra-eng'] = (DATA_URL + 'fra-eng.zip', '94646ad1522d915e7b0f9296181140edcf86a4f5')
@@ -64,7 +64,7 @@ def preprocess_nmt(text):
     out = [' ' + char if i > 0 and no_space(char, text[i - 1]) else char for i, char in enumerate(text)]
     return ''.join(out)
 
-# 9.5.2  词元化
+# source: 9.5.2  词元化
 def tokenize_nmt(text, num_examples=None):
     """词元化“英语－法语”数据数据集"""
     source, target = [], []
@@ -77,7 +77,7 @@ def tokenize_nmt(text, num_examples=None):
             target.append(parts[1].split(' '))
     return source, target
 
-# 9.5.3  词表
+# source: 9.5.3  词表
 class Vocab:
     """文本词表"""
     def __init__(self, tokens=None, min_freq=0, reserved_tokens=None):
@@ -127,7 +127,7 @@ class Vocab:
             tokens = [token for line in tokens for token in line]
         return collections.Counter(tokens)
 
-# 9.5.4  加载数据集
+# source: 9.5.4  加载数据集
 def truncate_pad(line, num_steps, padding_token):
     """截断或填充文本序列"""
     if len(line) > num_steps:
@@ -143,7 +143,7 @@ def build_array_nmt(lines, vocab, num_steps):
     valid_len = (array != vocab['<pad>']).type(torch.int32).sum(1)
     return array, valid_len
 
-# 9.5.5  训练模型
+# source: 9.5.5  训练模型
 def load_array(data_arrays, batch_size, is_train=True):
     """Construct a PyTorch data iterator."""
     dataset = data.TensorDataset(*data_arrays)
@@ -170,7 +170,7 @@ def try_gpu(i=0):
     return torch.device('cpu')
 # --------------------------------------------------------------------------- #
 
-# 9.6.1  编码器
+# source: 9.6.1  编码器
 class Encoder(nn.Module):
     """编码器-解码器架构的基本编码器接口"""
     def __init__(self, **kwargs):
@@ -179,7 +179,7 @@ class Encoder(nn.Module):
     def forward(self, X, *args):
         raise NotImplementedError
 
-# 9.6.2  解码器
+# source: 9.6.2  解码器
 class Decoder(nn.Module):
     """编码器-解码器架构的基本解码器接口"""
     def __init__(self, **kwargs):
@@ -191,7 +191,7 @@ class Decoder(nn.Module):
     def forward(self, X, state):
         raise NotImplementedError
 
-# 9.6.3  合并编码器和解码器
+# source: 9.6.3  合并编码器和解码器
 class EncoderDecoder(nn.Module):
     """编码器-解码器架构的基类"""
     def __init__(self, encoder, decoder, **kwargs):
@@ -205,5 +205,243 @@ class EncoderDecoder(nn.Module):
         return self.decoder(dec_X, dec_state)
 
 
+# source: 9.7.1  编码器
+class Seq2SeqEncoder(Encoder):
+    """用于序列到序列学习的循环神经网络编码器"""
+    def __init__(self, vocab_size, embed_size, num_hiddens, num_layers, dropout=0, **kwargs):
+        super(Seq2SeqEncoder, self).__init__(**kwargs)
+        # 嵌入层
+        self.embedding = nn.Embedding(vocab_size, embed_size)
+        self.rnn = nn.GRU(embed_size, num_hiddens, num_layers, dropout=dropout)
+
+    def forward(self, X, *args):
+        # 此时, X shape = (batch_size, num_steps)
+        X = self.embedding(X)
+        # 此时, X shape = (batch_size, num_steps, embed_size)
+        # 在循环神经网络模型中，第一个轴对应于时间步
+        X = X.permute(1, 0, 2)
+        # 此时, X shape = (num_steps, batch_size, embed_size)
+        # 如果未提及状态，则默认为0
+        output, state = self.rnn(X) # todo: 为什么这里的输入只有 X, 没有 state, 缺失 state 时, 会自动生成并初始化?
+        # output shape = (num_steps, batch_size, num_hiddens)
+        # state shape = (num_layers, batch_size, num_hiddens)
+        return output, state
+
+# source: 9.7.2  解码器
+class Seq2SeqDecoder(Decoder):
+    """用于序列到序列学习的循环神经网络解码器"""
+    def __init__(self, vocab_size, embed_size, num_hiddens, num_layers, dropout=0, **kwargs):
+        super(Seq2SeqDecoder, self).__init__(**kwargs)
+        self.embedding = nn.Embedding(vocab_size, embed_size)
+        self.rnn = nn.GRU(embed_size + num_hiddens, num_hiddens, num_layers, dropout=dropout)
+        self.dense = nn.Linear(num_hiddens, vocab_size)
+
+    def init_state(self, enc_outputs, *args):
+        return enc_outputs[1]
+
+    def forward(self, X, state):
+        # 此时, X shape = (batch_size, num_steps)
+        #   state shape = (num_layers, batch_size, num_hiddens)
+        X = self.embedding(X).permute(1, 0, 2)
+        # 此时, X shape = (num_steps, batch_size, embed_size)
+
+        # 广播context，使其具有与X相同的num_steps
+        context = state[-1].repeat(X.shape[0], 1, 1)
+        # context shape = (num_steps, batch_size, num_hiddens)
+        X_and_context = torch.cat((X, context), 2)
+        # X_and_context shape = (num_steps, batch_size, embed_size + num_hiddens)
+        output, state = self.rnn(X_and_context, state)
+        # output shape = (num_steps, batch_size, num_hiddens)
+        # state shape = (num_layers, batch_size, num_hiddens)
+        output = self.dense(output).permute(1, 0, 2)
+        # output shape = (batch_size, num_steps, vocab_size)
+        return output, state
+
+# source: 9.7.3  损失函数
+def sequence_mask(X, valid_len, value=0):
+    """在序列中屏蔽不相关的项"""
+    maxlen = X.size(1)
+    # print("mask left =", torch.arange((maxlen), dtype=torch.float32, device=X.device)[None, :])
+    # print("mask right =", valid_len[:, None])
+    mask = torch.arange((maxlen), dtype=torch.float32, device=X.device)[None, :] < valid_len[:, None]
+    # print("mask =", mask)
+    X[~mask] = value
+    return X
+
+class MaskedSoftmaxCELoss(nn.CrossEntropyLoss):
+    """带遮蔽的softmax交叉熵损失函数"""
+    # pred的形状：(batch_size,num_steps,vocab_size)
+    # label的形状：(batch_size,num_steps)
+    # valid_len的形状：(batch_size,)
+    def forward(self, pred, label, valid_len):
+        weights = torch.ones_like(label)
+        weights = sequence_mask(weights, valid_len)
+        # print("weights =", weights)
+        self.reduction='none'
+        unweighted_loss = super(MaskedSoftmaxCELoss, self).forward( # 这句什么意思？
+            pred.permute(0, 2, 1), label)
+        # print("unweighted_loss =", unweighted_loss)
+        weighted_loss = (unweighted_loss * weights).mean(dim=1)
+        return weighted_loss
+    
 
 
+# source: 8.5.5. 梯度裁剪
+def grad_clipping(net, theta):
+    """裁剪梯度"""
+    if isinstance(net, nn.Module):
+        params = [p for p in net.parameters() if p.requires_grad]
+    else:
+        params = net.params
+    norm = torch.sqrt(sum( torch.sum((p.grad ** 2)) for p in params ))
+    if norm > theta:
+        for param in params:
+            param.grad[:] *= theta / norm
+
+
+import time, numpy
+class Timer:
+    """Record multiple running times."""
+    def __init__(self):
+        self.times = []
+        self.start()
+
+    def start(self):
+        """Start the timer."""
+        self.tik = time.time()
+
+    def stop(self):
+        """Stop the timer and record the time in a list."""
+        self.times.append(time.time() - self.tik)
+        return self.times[-1]
+
+    def avg(self):
+        """Return the average time."""
+        return sum(self.times) / len(self.times)
+
+    def sum(self):
+        """Return the sum of time."""
+        return sum(self.times)
+
+    def cumsum(self):
+        """Return the accumulated time."""
+        return numpy.array(self.times).cumsum().tolist()
+
+class Accumulator:
+    """For accumulating sums over `n` variables."""
+    def __init__(self, n):
+        self.data = [0.0] * n
+
+    def add(self, *args):
+        self.data = [a + float(b) for a, b in zip(self.data, args)]
+
+    def reset(self):
+        self.data = [0.0] * len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+# source: 9.7.4  训练
+def train_seq2seq(net, data_iter, lr, num_epochs, tgt_vocab, device):
+    """训练序列到序列模型"""
+    def xavier_init_weights(m):
+        if type(m) == nn.Linear:
+            nn.init.xavier_uniform_(m.weight)
+        if type(m) == nn.GRU:
+            for param in m._flat_weights_names:
+                if "weight" in param:
+                    nn.init.xavier_uniform_(m._parameters[param])
+
+    net.apply(xavier_init_weights)
+    net.to(device)
+    optimizer = torch.optim.Adam(net.parameters(), lr=lr)
+    loss = MaskedSoftmaxCELoss()
+    net.train()
+    for epoch in range(num_epochs):
+        timer = Timer()
+        metric = Accumulator(2)  # 训练损失总和，词元数量
+        for batch in data_iter:
+            optimizer.zero_grad()
+            X, X_valid_len, Y, Y_valid_len = [x.to(device) for x in batch]
+            bos = torch.tensor([tgt_vocab['<bos>']] * Y.shape[0],
+                          device=device).reshape(-1, 1)
+            dec_input = torch.cat([bos, Y[:, :-1]], 1)  # 强制教学
+            Y_hat, _ = net(X, dec_input, X_valid_len)
+            l = loss(Y_hat, Y, Y_valid_len)
+            l.sum().backward()    # 损失函数的标量进行“反向传播”
+            grad_clipping(net, 1) # 梯度裁剪
+            num_tokens = Y_valid_len.sum()
+            optimizer.step()
+            with torch.no_grad():
+                metric.add(l.sum(), num_tokens)
+
+    print(f'loss {metric[0] / metric[1]:.3f}, {metric[1] / timer.stop():.1f} '
+        f'tokens/sec on {str(device)}')
+    
+embed_size, num_hiddens, num_layers, dropout = 32, 32, 2, 0.1
+batch_size, num_steps = 64, 10
+lr, num_epochs, device = 0.005, 300, try_gpu()
+
+train_iter, src_vocab, tgt_vocab = load_data_nmt(batch_size, num_steps) # load_data_nmt() 需加载"9.5 机器翻译与数据集"
+encoder = Seq2SeqEncoder(len(src_vocab), embed_size, num_hiddens, num_layers, dropout)
+decoder = Seq2SeqDecoder(len(tgt_vocab), embed_size, num_hiddens, num_layers, dropout)
+net = EncoderDecoder(encoder, decoder)
+train_seq2seq(net, train_iter, lr, num_epochs, tgt_vocab, device)
+
+# source: 9.7.5  预测
+
+def predict_seq2seq(net, src_sentence, src_vocab, tgt_vocab, num_steps,
+                    device, save_attention_weights=False):
+    """序列到序列模型的预测"""
+    # 在预测时将net设置为评估模式
+    net.eval()
+    src_tokens = src_vocab[src_sentence.lower().split(' ')] + [src_vocab['<eos>']]
+    enc_valid_len = torch.tensor([len(src_tokens)], device=device)
+    src_tokens = truncate_pad(src_tokens, num_steps, src_vocab['<pad>'])
+    # 添加批量轴
+    enc_X = torch.unsqueeze(
+        torch.tensor(src_tokens, dtype=torch.long, device=device), dim=0)
+    enc_outputs = net.encoder(enc_X, enc_valid_len)
+    dec_state = net.decoder.init_state(enc_outputs, enc_valid_len)
+    # 添加批量轴
+    dec_X = torch.unsqueeze(torch.tensor(
+        [tgt_vocab['<bos>']], dtype=torch.long, device=device), dim=0)
+    output_seq, attention_weight_seq = [], []
+    for _ in range(num_steps):
+        Y, dec_state = net.decoder(dec_X, dec_state)
+        # 我们使用具有预测最高可能性的词元，作为解码器在下一时间步的输入
+        dec_X = Y.argmax(dim=2)
+        pred = dec_X.squeeze(dim=0).type(torch.int32).item()
+        # 保存注意力权重（稍后讨论）
+        if save_attention_weights:
+            attention_weight_seq.append(net.decoder.attention_weights)
+        # 一旦序列结束词元被预测，输出序列的生成就完成了
+        if pred == tgt_vocab['<eos>']:
+            break
+        output_seq.append(pred)
+    return ' '.join(tgt_vocab.to_tokens(output_seq)), attention_weight_seq
+
+# source: 9.7.6  预测序列的评估
+def bleu(pred_seq, label_seq, k):
+    """计算BLEU"""
+    pred_tokens, label_tokens = pred_seq.split(' '), label_seq.split(' ')
+    len_pred, len_label = len(pred_tokens), len(label_tokens)
+    score = math.exp(min(0, 1 - len_label / len_pred))
+    for n in range(1, k + 1):
+        num_matches, label_subs = 0, collections.defaultdict(int)
+        for i in range(len_label - n + 1):
+            label_subs[' '.join(label_tokens[i: i + n])] += 1
+        for i in range(len_pred - n + 1):
+            if label_subs[' '.join(pred_tokens[i: i + n])] > 0:
+                num_matches += 1
+                label_subs[' '.join(pred_tokens[i: i + n])] -= 1
+        score *= math.pow(num_matches / (len_pred - n + 1), math.pow(0.5, n))
+    return score
+
+
+engs = ['go .', "i lost .", 'he\'s calm .', 'i\'m home .']
+fras = ['va !', 'j\'ai perdu .', 'il est calme .', 'je suis chez moi .']
+for eng, fra in zip(engs, fras):
+    translation, attention_weight_seq = predict_seq2seq(
+        net, eng, src_vocab, tgt_vocab, num_steps, device)
+    print(f'{eng} => {translation}, bleu {bleu(translation, fra, k=2):.3f}')
